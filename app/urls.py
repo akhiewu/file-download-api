ab77e664dea0409e7d81237c07d2fc1b0b5a763f
akhiewu <2016-2-60-142@std.ewubd.edu>
2022-03-03 18:24:11 +0600

    first commit

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.index),
+    path('pdf_view/', views.ViewPDF.as_view(), name="pdf_view"),
+    path('pdf_download/', views.DownloadPDF.as_view(), name="pdf_download"),
+    path('pdf/', views.GeneratePDF.as_view()),
+    
+]
---
+++ app/views.py
@@ -0,0 +1,126 @@
+#----------------------------------- AKHI---------------------------------------------#
+
+from django.shortcuts import render
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views import View
+from xhtml2pdf import pisa
+
+def render_to_pdf(template_src, context_dict={}):
+	template = get_template(template_src)
+	html  = template.render(context_dict)
+	result = BytesIO()
+	pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+	if not pdf.err:
+		return HttpResponse(result.getvalue(), content_type='application/pdf')
+	return None
+
+
+data = {
+	"company": "Dennnis Ivanov Company",
+	"address": "123 Street name",
+	"city": "Vancouver",
+	"state": "WA",
+	"zipcode": "98663",
+
+
+	"phone": "555-555-2345",
+	"email": "youremail@dennisivy.com",
+	"website": "dennisivy.com",
+	}
+
+#Opens up page as PDF
+class ViewPDF(View):
+	def get(self, request, *args, **kwargs):
+
+		pdf = render_to_pdf('app/pdf_template.html', data)
+		return HttpResponse(pdf, content_type='application/pdf')
+
+
+#Automaticly downloads to PDF file
+class DownloadPDF(View):
+	def get(self, request, *args, **kwargs):
+		
+		pdf = render_to_pdf('app/pdf_template.html', data)
+
+		response = HttpResponse(pdf, content_type='application/pdf')
+		filename = "Invoice_%s.pdf" %("12341231")
+		content = "attachment; filename='%s'" %(filename)
+		response['Content-Disposition'] = content
+		return response
+
+
+
+def index(request):
+	context = {}
+	return render(request, 'app/index.html', context)
+
+#-----------------------------------------------------------------------------
+#                                 Setu
+#-----------------------------------------------------------------------------
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+
+from .utils import render_to_pdf #created in step 4
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('invoice.html')
+        context = {
+            "invoice_id": 123,
+            "customer_name": "John Cooper",
+            "amount": 1399.99,
+            "today": "Today",
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('invoice.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" %("12341231")
+            content = "inline; filename='%s'" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" %(filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
+   #------------------------------------------------------------------------------------- 
+    
+    # def process_report(request):
+    
+
+    #     if request.method == 'POST':
+    #         report_file = request.FILES.pop('file')[0]
+
+    #         merged_pdf, namefinal = render_to_pdf(report_file)
+
+    #         response = HttpResponse(content_type='application/pdf')
+
+    #         response['Content-Disposition'] = 'attachment; filename="{}"'.format(namefinal)
+    #         response.write(merged_pdf.getvalue())
+
+    #         return response
+
+    #     else:
+    #         return HttpResponse("Must be Post")
+    
+    
+import mimetypes
+
+
+
+def download_file(request):
+        # fill these variables with real values
+        fl_path = '/file/path'
+        filename = 'downloaded_file_name.extension'
+
+        fl = open(fl_path, 'r')
+        mime_type, _ = mimetypes.guess_type(fl_path)
+        response = HttpResponse(fl, content_type=mime_type)
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        
+        return response
+                
+        
